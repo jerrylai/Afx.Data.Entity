0c424cb116b7383a6aeeeb75765152c76b7ffd5e
jerrylai <jerrylai@aliyun.com>
2021-02-18 12:17:23 +0800

    13.2.1

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("13.2.0")]
-[assembly: AssemblyFileVersion("13.2.0")]
+[assembly: AssemblyVersion("13.2.1")]
+[assembly: AssemblyFileVersion("13.2.1")]
